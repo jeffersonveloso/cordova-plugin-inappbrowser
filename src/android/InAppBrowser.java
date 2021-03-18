c53bbf5eee59a81d337967f6f990d0f8793d6c36
Jefferson Veloso <jeffersonveloso_6@hotmail.com>
2021-03-18 18:49:36 -0300

    Added modifications to enable the photo gallery and camera intent when clicking on the input file, added image optimization to reduce the size

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1035,7 +1035,7 @@ public class InAppBrowser extends CordovaPlugin {
 
                             }
 
-                            //intent galery
+                            //intent gallery
                             Intent pickPhoto = new Intent(Intent.ACTION_PICK,
                             MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                             pickPhoto.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
